1de4f08f8043ed4fffcf42f06e14cde94f07334f
Leroy Chirodza <chirodzaleroy01@gmail.com>
2026-01-29 23:20:32 +0300

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,53 @@
+var newLine = true;  //Boolean variable. Determines if the next thing the user types be on a new line
+                var value1;
+                var currentOperator;
+
+
+                //Event handler for when any digit button is pressed 
+                function digitBtnPressed(button){
+
+                    if(newLine){
+                        document.getElementById("inputBox").value = button;
+                        newLine = false;
+                     } else{
+                    var currentValue = document.getElementById("inputBox").value;
+                    document.getElementById("inputBox").value= currentValue + button;
+                        }
+                }
+                //Event handler for when the AC button is pressed 
+                function btnACPressed(){
+                    document.getElementById("inputBox").value = 0;
+                    newLine = true;
+                }
+                function operatorBtnPressed(operator){
+                 currentOperator = operator;
+                 value1 = parseInt(document.getElementById("inputBox").value);
+                 newLine = true;
+                }
+
+                //Event handler for equals to button
+                function equalsBtnPressed(){
+                    var value2 = parseInt(document.getElementById("inputBox").value);
+                    var finalTotal;
+
+                    switch(currentOperator){
+                        case "+":
+                            finalTotal = value1 + value2;
+                            break;
+                         case "-":
+                            finalTotal = value1 - value2;
+                            break;
+                         case "/":
+                             finalTotal = value1 / value2;
+                             break;
+                         case "x":
+                             finalTotal = value1 * value2;
+                             break;
+                    }
+                    document.getElementById("inputBox").value = finalTotal;
+
+                    value1 =0;
+                    newLine=true;
+                }
+                
+        
